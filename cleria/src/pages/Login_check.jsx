7f4704a65b589e4c54625e0dc53c31979190113a
Aditya7863 <119505076+Aditya7863@users.noreply.github.com>
2025-05-01 17:18:21 +0530

    a

--- cleria/src/pages/Login_check.jsx
+++ cleria/src/pages/Login_check.jsx
@@ -2,23 +2,22 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const Login_check = () => {
-  const navigate=useNavigate()
-   return( <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6">
-      <div className='flex-col justify-center items-center'>
+  const navigate = useNavigate()
+  return (<div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-6">
+    <div className='flex-col justify-center items-center'>
       <h2 className="text-4xl font-bold text-blue-600 mb-4 justify-center items-center">Login</h2>
       <ul>
-                <li><button className='text-white font-normal! bg-gradient-to-br w-full text-2xl! hover:cursor-pointer from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>User</button></li>
-                <li><button  onClick={() => navigate("/login/admin")}  className=' w-full font-normal! text-2xl! hover:cursor-pointer text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Admin</button></li>
-                <li><button className='w-full text-2xl! font-normal! hover:cursor-pointer text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Staff </button></li>
-                <li><button className='w-full text-2xl!  font-normal! hover:cursor-pointer text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Doctor</button></li>
+        <li><button className='text-white font-normal! bg-gradient-to-br w-full text-2xl! hover:cursor-pointer from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>User</button></li>
+        <li><button onClick={() => navigate("/login/admin")} className=' w-full font-normal! text-2xl! hover:cursor-pointer text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Admin</button></li>
+        <li><button className='w-full text-2xl! font-normal! hover:cursor-pointer text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Staff </button></li>
+        <li><button className='w-full text-2xl!  font-normal! hover:cursor-pointer text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2'>Doctor</button></li>
 
-            </ul>
-            <p className="mt-4">Don't have an account? <span onClick={()=>navigate("/register")} className='text-blue-500 hover:cursor-pointer'>Register</span> </p>
-            </div>
+      </ul>
+      <p className="mt-4">Don't have an account? <span onClick={() => navigate("/register")} className='text-blue-500 hover:cursor-pointer'>Register</span> </p>
     </div>
-    )
+  </div>
+  )
 }
 
-export default Login_check  
+export default Login_check
 
-           
